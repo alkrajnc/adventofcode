ecc4f1c0f85524e73e4195482e849ffdf9aa75f3
alkrajnc <aljazkrajnc8@gmail.com>
2025-12-03 16:47:49 +0100

    Day 3 Part Two

--- day3/src/Main.kt
+++ day3/src/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.math.BigInteger
 
 
 fun getNumbers(str: String): MutableList<Int> {
@@ -10,29 +11,88 @@ fun getNumbers(str: String): MutableList<Int> {
 }
 
 
+class Max(var value: Int, var index: Int): Comparable<Max> {
+    override fun compareTo(other: Max): Int {
+        return index.compareTo(other.index)
+    }
+
+    override fun toString(): String {
+        return "val: ${value}, index: ${index}"
+    }
+}
+
+
+
+fun getMaxes(nums: MutableList<Int>): MutableList<Max> {
+    val maxes = mutableListOf<Max>()
+
+    val indexed = mutableListOf<Max>()
+    var i = 0
+    for(num in nums) {
+       indexed.add(Max(num, i))
+        i++
+    }
+    indexed.sortWith(compareBy<Max> { it.value })
+
+
+    for(num in  indexed.slice(0..12)) {
+        maxes.add(num)
+    }
+
+
+
+    return maxes;
+}
+
+
 
 fun main() {
-    var solution: Int = 0
+    var solution: BigInteger = BigInteger.ZERO
 
     File("/home/aljaz/projects/adventofcode/day3/src/input.txt").forEachLine {
         val nums: MutableList<Int> = getNumbers(it)
-        var max: Int = 0;
-        var secondMax: Int = 0
-        var maxIndex: Int = 0
+//        var max: Int = 0;
+//        var secondMax: Int = 0
+//        var maxIndex: Int = 0
+//
+//        for (i in 0..<nums.size-1) {
+//            if(nums[i] > max) {
+//                max = nums[i]
+//                maxIndex = i
+//            }
+//        }
+//
+//        for(i in maxIndex+1..<nums.size) {
+//            if(nums[i] > secondMax) {
+//                secondMax = nums[i]
+//            }
+//        }
+//        solution += max*10+secondMax;
 
-        for (i in 0..<nums.size-1) {
-            if(nums[i] > max) {
-                max = nums[i]
-                maxIndex = i
-            }
-        }
+//        var intermidiate: Long = 0
+//        for(max in maxes) {
+//            println("val: ${max.value} index: ${max.index}");
+//            intermidiate+= max.value * Math.unsignedPowExact(10, maxIndex - max.index - 1)
+//        }
 
-        for(i in maxIndex+1..<nums.size) {
-            if(nums[i] > secondMax) {
-                secondMax = nums[i]
-            }
+
+
+        val maxes = getMaxes(nums)
+
+        val maxIndex = 0
+        
+
+
+        var intermediate = BigInteger.ZERO;
+        for(max in maxes) {
+            println(max.toString())
+            val powerOfTen: BigInteger = BigInteger.TEN.pow(maxIndex - max.index);
+            intermediate = intermediate.add(
+                BigInteger.valueOf(max.value.toLong()).multiply(powerOfTen)
+            );
         }
-        solution += max*10+secondMax;
+        println(intermediate)
+        solution+=intermediate
     }
     println(solution)
 }
